4eef55457482282e5c3c5c4bcef802e0b1ae4d3c
abdulrg01 <abdulrg01@gmail.com>
2025-04-12 12:52:31 -0400

    logo name

--- client/components/Logos.jsx
+++ client/components/Logos.jsx
@@ -7,33 +7,65 @@ export default function Logos() {
       <div className="group relative w-full px-1">
         <div className="relative py-6">
           <InfiniteSlider speedOnHover={20} speed={40} gap={40}>
-            <div className="flex">
-              <Image src="/pharmacy/pain.jpeg" alt="" width={150} height={150} className="rounded-lg" />
+            <div className="flex flex-col items-center">
+              <Image
+                src="/pharmacy/pain.jpeg"
+                alt=""
+                width={150}
+                height={150}
+                className="rounded-lg"
+              />
+              <p className="text-white font-semibold">Pain</p>
             </div>
-            <div className="flex">
-              <Image src="/pharmacy/nasal.jpeg" alt="" width={150} height={150} className="rounded-lg" />
+            <div className="flex flex-col items-center">
+              <Image
+                src="/pharmacy/nasal.jpeg"
+                alt=""
+                width={150}
+                height={150}
+                className="rounded-lg"
+              />
+              <p className="text-white font-semibold">Nasal</p>
             </div>
-            <div className="flex">
-              <Image src="/pharmacy/eyedrop.jpeg" alt="" width={150} height={150} className="rounded-lg" />
+            <div className="flex flex-col items-center">
+              <Image
+                src="/pharmacy/eyedrop.jpeg"
+                alt=""
+                width={150}
+                height={150}
+                className="rounded-lg"
+              />
+              <p className="text-white font-semibold">Eye drop</p>
             </div>
-            <div className="flex">
-              <Image src="/pharmacy/cold.jpeg" alt="" width={150} height={150} className="rounded-lg" />
+            <div className="flex flex-col items-center">
+              <Image
+                src="/pharmacy/cold.jpeg"
+                alt=""
+                width={150}
+                height={150}
+                className="rounded-lg"
+              />
+              <p className="text-white font-semibold">Cold</p>
             </div>
-            <div className="flex">
+            <div className="flex flex-col items-center">
               <Image
                 src="/pharmacy/rashes.jpeg"
                 alt=""
                 width={150}
-                height={150} className="rounded-lg"
+                height={150}
+                className="rounded-lg"
               />
+              <p className="text-white font-semibold">Rashes</p>
             </div>
-            <div className="flex">
+            <div className="flex flex-col items-center">
               <Image
                 src="/pharmacy/digestion.jpeg"
                 alt=""
                 width={150}
-                height={150} className="rounded-lg"
+                height={150}
+                className="rounded-lg"
               />
+              <p className="text-white font-semibold">Digestion</p>
             </div>
           </InfiniteSlider>
         </div>
