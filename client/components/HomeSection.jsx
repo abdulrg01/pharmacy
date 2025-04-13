e70b64cb8fcd0e3a62728fb67cbcf2c886de1c9d
abdulrg01 <abdulrg01@gmail.com>
2025-04-13 04:59:55 -0400

    images

--- client/components/HomeSection.jsx
+++ client/components/HomeSection.jsx
@@ -3,13 +3,13 @@ import Logos from "./Logos";
 
 export default function HomeSection() {
   return (
-    <div className="py-3 flex flex-col">
-      <div className="bg-[#035e85] flex-1 pb-5">
-        <div className="container mx-auto px-4 flex flex-col md:flex-row items-center">
-          <div className="w-full md:w-1/2 flex justify-center mt-5">
+    <div className="py-5 flex flex-col">
+      <div className="bg-[#035e85] flex-1">
+        <div className="container mx-auto flex flex-col md:flex-row items-center justify-center">
+          <div className="w-full md:w-1/2 flex justify-center pt-14">
             <AnimatedBurger />
           </div>
-          <div className="w-full md:w-1/2 space-y-5 -mt-20 md:mt-0">
+          <div className="w-full md:w-1/2 space-y-2 -mt-20 md:mt-0">
             <h1 className="text-5xl md:text-6xl max-sm:text-center font-bold text-white">
               Sauki delivery
             </h1>
